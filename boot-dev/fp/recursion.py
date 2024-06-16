33373a3ee97fa2b1e1dfde9771b5745a4c613eab
Victor Salles <vsallesa@gmail.com>
2024-06-16 16:45:20 -0300

    update functional programming course

--- boot-dev/fp/recursion.py
+++ boot-dev/fp/recursion.py
@@ -18,3 +18,37 @@ def zipmap(keys, values):
     res = zipmap(keys[1:], values[1:])
     res[keys[0]] = values[0]
     return res
+
+def sum_nested_list(lst):
+    size = 0
+    for item in lst:
+        if not isinstance(item, list): size += item
+        else: size += sum_nested_list(item)
+    return size
+
+def list_files(current_filetree, current_path=""):
+    file_paths = list()
+
+    for node in current_filetree:
+        nested = current_filetree[node]
+        if nested == None: file_paths.append(f"{current_path}/{node}")
+        else: 
+            file_paths.extend(list_files(nested, f"{current_path}/{node}"))
+
+    return file_paths
+
+def count_nested_levels(nested_documents, target_document_id, level=1):
+    for document_id in nested_documents:
+        if document_id == target_document_id:
+            return level
+        found_level = count_nested_levels(
+            nested_documents[document_id], target_document_id, level + 1
+        )
+        if found_level != -1:
+            return found_level
+    return -1
+
+def reverse_string(s):
+    if len(s) == 0:
+        return s
+    return reverse_string(s[1:]) + s[0]
